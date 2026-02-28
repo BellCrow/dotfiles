c20f0a3b6283f74116232383b1bfd2e21b712723
Joshua Krieger <joshua.krieger@nikon-slm-solutions.com>
2026-02-28 19:22:57 +0100

    - add binds for signatur ehelper and file location copy

--- .config/nvim/lua/editing/binds.lua
+++ .config/nvim/lua/editing/binds.lua
@@ -12,9 +12,18 @@ vim.keymap.set("n", "<leader>rn", buf.rename)
 vim.keymap.set("n", "<leader>ca", buf.code_action)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gD", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("i", "<C-s>", function()
+    vim.lsp.buf.signature_help({ anchor_bias = "above" })
+end)
 
 -- so i can paste multiple times the same text
 vim.keymap.set({ "n", "v" }, "<leader>p", '"_dP')
 
 -- shortcut for selecting words
 vim.keymap.set("v", "v", "iw")
+
+-- copy absolute path of current file(or buffer) to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>l", function()
+    vim.fn.setreg("+", vim.fn.expand("%:p"))
+end)
