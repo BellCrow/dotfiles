72df25f8010352981b9082ac0f07964a6f83a6f0
Joshua Krieger <joshua.krieger@nikon-slm-solutions.com>
2026-02-28 19:11:53 +0100

    - add neogen (documentation generation tool)

---
+++ .config/nvim/lua/editing/plugins/neogen.lua
@@ -0,0 +1,13 @@
+return {
+    "danymat/neogen",
+    config = function()
+        require("neogen").setup({})
+    end,
+    -- languages = {
+    --     cs = {
+    --         template = {
+    --             annotation_convention = "xmldoc",
+    --         },
+    --     },
+    -- },
+}
